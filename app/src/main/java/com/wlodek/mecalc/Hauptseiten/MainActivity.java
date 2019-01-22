e5f86f7fdfa2a9a5d3de73899b0b5a6633af712b
Konrad Wlodek <wlo17090@spengergasse.at>
2019-01-22 15:46:16 +0100

    Initial commit

---
+++ app/src/main/java/com/wlodek/mecalc/Hauptseiten/MainActivity.java
@@ -0,0 +1,18 @@
+package com.wlodek.mecalc.Hauptseiten;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wlodek.mecalc.R;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/wlodek/mecalc/Hauptseiten/Tagebuch.java
@@ -0,0 +1,15 @@
+package com.wlodek.mecalc.Hauptseiten;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wlodek.mecalc.R;
+
+public class Tagebuch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tagebuch);
+    }
+}
